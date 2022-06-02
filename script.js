5fe7348ad714f0dd4bd2d1208dedda086e7252b1
whimsicaltestical <jamgriffin7@gmail.com>
2022-06-02 10:57:36 -0500

    Add function to generate CPU player action Created the computerPlay() function to generate a random answer resp- onse between Rock, Paper, and Scissors

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello");
+function computerPlay() {  // generates the CPU player's answer
+  let random = ["Rock", "Paper", "Scissors"];
+  let compAns = random[Math.floor(Math.random() * random.length)];
+  return compAns;      
+}
