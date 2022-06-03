cc93957a2aacf64053d20ff076d82b9ace352ed0
whimsicaltestical <jamgriffin7@gmail.com>
2022-06-03 11:41:40 -0500

    Add function to play 1 round
    Created a function to play one round of Rock, Paper, Scissors
    Function takes the string parameter passed and converts it to upperc-
    ase and then compares to the computer generated value and returns
    the result of the comparison as either a win or a loss for the player

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-function computerPlay() {  // generates the CPU player's answer
-  let random = ["Rock", "Paper", "Scissors"];
-  let compAns = random[Math.floor(Math.random() * random.length)];
-  return compAns;      
+function computerPlay() {  // generates the CPU player's answer randomly
+  let random = ["ROCK", "PAPER", "SCISSORS"];
+  let computerHand = random[Math.floor(Math.random() * random.length)];
+  return computerHand;      
 }
+
+// This functions plays 1 round of rock, paper, scissors
+function playRound(playerSelection, computerSelection = computerPlay()) {
+  let playerHand = playerSelection.toUpperCase();
+  let compHand = computerSelection;
+  console.log(playerHand);
+  console.log(computerSelection);
+  if (playerHand === compHand) {
+      return "Draw! Play again"
+  } else if (playerHand === "ROCK" && compHand === "SCISSORS") {
+      return "You win! Rock beats Scissors!";
+  } else if (playerHand === "ROCK" && compHand === "PAPER") {
+      return "You lose! Paper beats Rock!";
+  } else if (playerHand === "SCISSORS" && compHand === "PAPER") {
+      return "You win! Scissors beats Paper!";
+  } else if (playerHand === "SCISSORS" && compHand === "ROCK") {
+      return "You lose! Rock beats Scissors!";
+  } else if (playerHand === "PAPER" && compHand === "ROCK") {
+      return "You win! Paper beats Rock!";
+  } else if (playerHand === "PAPER" && compHand === "SCISSORS") {
+      return "You lose! Scissors beats Paper!";
+  } else {
+      return "Please input either Rock, Paper, or Scissors.";
+  }
+}
+
+const computerSelection = computerPlay();
